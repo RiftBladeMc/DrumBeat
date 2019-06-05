8bc3bc543dfdb02e39f6628662a74b2e09cd716a
BuildTools <unconfigured@null.spigotmc.org>
2019-06-05 00:58:55 -0500

    Initial commit

---
+++ src/com/drumbeatbillings/drumbeat/Database.java
@@ -0,0 +1,32 @@
+package com.drumbeatbillings.drumbeat;
+
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Database implements AutoCloseable {
+    private final Connection connection;
+    public Database(File file) throws SQLException {
+        connection = DriverManager.getConnection("jdbc:sqlite:" + (file.isDirectory() ? file.getAbsolutePath() + File.separatorChar + "drumbeat.db" : file.getAbsolutePath()));
+    }
+
+    public Database(String address, int port, String database, String username, String password) throws SQLException {
+        connection = DriverManager.getConnection("jdbc:mysql://" + address + ':' + (port == 0 ? 3306 : port) + '/' + database, username, password);
+    }
+
+    public Database(String address, int port, String database) throws SQLException {
+        connection = DriverManager.getConnection("jdbc:mysql://" + address + ':' + (port == 0 ? 3306 : port) + '/' + database);
+    }
+
+    private void init() throws SQLException {
+        try (Statement stmt = connection.createStatement()) {
+            stmt.execute("CREATE TABLE DrumBeat ('id' TINYINT UNSIGNED PRIMARY KEY AUTOINCREMENT, 'first' TEXT NOT NULL, 'last' TEXT NOT NULL, 'company' TEXT, 'email' TEXT, 'address' TEXT, 'phone' TEXT);");
+        }
+    }
+
+    public void close() throws Exception {
+        connection.close();
+    }
+}
---
+++ src/com/drumbeatbillings/drumbeat/DrumBeat.java
@@ -0,0 +1,71 @@
+package com.drumbeatbillings.drumbeat;
+
+import javafx.scene.control.Accordion;
+import org.jdesktop.swingx.JXTaskPane;
+
+import javax.swing.*;
+import java.awt.*;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.sql.SQLException;
+import java.util.Objects;
+
+public class DrumBeat {
+    private static DrumBeat instance;
+
+    public static DrumBeat getInstance() {
+        return instance;
+    }
+
+    public static void main(String[] args) {
+        instance = new DrumBeat();
+    }
+
+    public void handleError(Exception e) {
+        StringWriter sw = new StringWriter();
+        try (PrintWriter pw = new PrintWriter(sw)) {
+            e.printStackTrace(pw);
+        }
+        String error = e.getMessage();
+        if (error == null) error = "An error has occurred";
+        JPanel err = new JPanel();
+        err.setLayout(new BoxLayout(err, BoxLayout.Y_AXIS));
+        JTextField textField = new JTextField(error);
+        textField.setMinimumSize(new Dimension(0, 20));
+        textField.setMaximumSize(new Dimension(Integer.MAX_VALUE, 20));
+        textField.setEditable(false);
+        textField.setBorder(null);
+        err.add(textField);
+
+        JTextField report = new JTextField("Please send the following info to support@drumbeatbillings.com or report it on our github.");
+        report.setMinimumSize(new Dimension(0, 20));
+        report.setMaximumSize(new Dimension(Integer.MAX_VALUE, 20));
+        report.setEditable(false);
+        report.setBorder(null);
+        err.add(report);
+
+        JXTaskPane taskPane = new JXTaskPane();
+        taskPane.setTitle("More Info");
+        JTextArea textArea = new JTextArea(sw.toString());
+        textArea.setBackground(Color.getColor("eeeeee"));
+        textArea.setEditable(false);
+        taskPane.add(textArea);
+        err.add(taskPane);
+        JOptionPane.showMessageDialog(null, err, error, JOptionPane.WARNING_MESSAGE);
+    }
+
+    private DrumBeat() {
+        try {
+            Objects.requireNonNull(null);
+        } catch (NullPointerException npe) {
+            handleError(npe);
+        }
+    }
+
+    public boolean isPro() {
+        return false;
+    }
+}
